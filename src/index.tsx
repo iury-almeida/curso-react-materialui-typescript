035001c5b0b0220a7c1a43e30f2387a3c43c8678
Iury Almeida <iury-almeida@hotmail.com>
2024-10-15 01:40:28 -0300

    added react router dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
   </React.StrictMode>
 );
